cd14ba33079b5ab0a92aa74550bae52b12e5d3d2
Ecioboy <ecioboy@163.com>
2022-10-11 19:33:47 +0800

    update

--- test.go
+++ test.go
@@ -1,27 +1,5 @@
 package main
 
-import "fmt"
-
-type Person struct {
-	name string
-	age  int
-}
-
-func (receiver Person) eat() {
-	fmt.Println("eat")
-}
-func (receiver Person) sleep() {
-	fmt.Println("sleep")
-}
-func (receiver Person) work() {
-	fmt.Println("work")
-}
 func main() {
-	p := Person{
-		name: "tom",
-		age:  20,
-	}
-	p.eat()
-	p.sleep()
-	p.work()
+
 }
